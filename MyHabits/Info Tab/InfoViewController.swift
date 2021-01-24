7630a5cef88342f66dddd49a31129cb5b3940124
Sergey <bindasov.sergey@gmail.com>
2021-01-24 18:04:47 +0300

    1. Удалил левый констрейнт у названия приложения в LaunchScreen 2. Добавил тройные ковычки 3. Удалил infoDescription.trailingAnchor.constraint 4. Добавил отступ  infoDescription.bottomAnchor.constraint

--- MyHabits/Info Tab/InfoViewController.swift
+++ MyHabits/Info Tab/InfoViewController.swift
@@ -20,7 +20,9 @@ class InfoViewController: UIViewController {
     
     private lazy var infoDescription: UILabel = {
         let description = UILabel()
-        description.text = "Прохождение этапов, за которые за 21 день вырабатывается привычка, подчиняется следующему алгоритму: \n\n1. Провести 1 день без обращения к старым привычкам, стараться вести себя так, как будто цель, загаданная в перспективу, находится на расстоянии шага. \n\n2. Выдержать 2 дня в прежнем состоянии самоконтроля. \n\n3. Отметить в дневнике первую неделю изменений и подвести первые итоги — что оказалось тяжело, что — легче, с чем еще предстоит серьезно бороться. \n\n4. Поздравить себя с прохождением первого серьезного порога в 21 день. За это время отказ от дурных наклонностей уже примет форму осознанного преодоления и человек сможет больше работать в сторону принятия положительных качеств. \n\n5. Держать планку 40 дней. Практикующий методику уже чувствует себя освободившимся от прошлого негатива и двигается в нужном направлении с хорошей динамикой. \n\n6. На 90-й день соблюдения техники все лишнее из «прошлой жизни» перестает напоминать о себе, и человек, оглянувшись назад, осознает себя полностью обновившимся. \n\nИсточник: psychbook.ru"
+        description.text = """
+Прохождение этапов, за которые за 21 день вырабатывается привычка, подчиняется следующему алгоритму: \n\n1. Провести 1 день без обращения к старым привычкам, стараться вести себя так, как будто цель, загаданная в перспективу, находится на расстоянии шага. \n\n2. Выдержать 2 дня в прежнем состоянии самоконтроля. \n\n3. Отметить в дневнике первую неделю изменений и подвести первые итоги — что оказалось тяжело, что — легче, с чем еще предстоит серьезно бороться. \n\n4. Поздравить себя с прохождением первого серьезного порога в 21 день. За это время отказ от дурных наклонностей уже примет форму осознанного преодоления и человек сможет больше работать в сторону принятия положительных качеств. \n\n5. Держать планку 40 дней. Практикующий методику уже чувствует себя освободившимся от прошлого негатива и двигается в нужном направлении с хорошей динамикой. \n\n6. На 90-й день соблюдения техники все лишнее из «прошлой жизни» перестает напоминать о себе, и человек, оглянувшись назад, осознает себя полностью обновившимся. \n\nИсточник: psychbook.ru
+"""
         description.font = UIFont.systemFont(ofSize: 17, weight: .regular)
         description.numberOfLines = 0
         return description
@@ -53,9 +55,8 @@ private extension InfoViewController {
             infoTitle.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 22),
             infoTitle.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
             infoDescription.topAnchor.constraint(equalTo: infoTitle.bottomAnchor, constant: 16),
-            infoDescription.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
             infoDescription.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
-            infoDescription.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            infoDescription.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16),
             infoDescription.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32)
         ]
         NSLayoutConstraint.activate(constraints)
